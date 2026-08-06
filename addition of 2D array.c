8976c484791deb6b24a80171ec1a5ad83cd7d29f
Varun Yadav <onlinevarunyadav@gmail.com>
2026-08-06 23:01:59 +0530

    This Program contains Addition of 2D array

---
+++ addition of 2D array.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int rows, cols, i, j;
+
+    printf("Enter rows and columns: ");
+    scanf("%d %d", &rows, &cols);
+
+    int a[rows][cols], b[rows][cols];
+
+    printf("Enter first matrix:\n");
+    for (i = 0; i < rows; i++)
+        for (j = 0; j < cols; j++)
+            scanf("%d", &a[i][j]);
+
+    printf("Enter second matrix:\n");
+    for (i = 0; i < rows; i++)
+        for (j = 0; j < cols; j++)
+            scanf("%d", &b[i][j]);
+
+    printf("Sum of matrices:\n");
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++)
+            printf("%d ", a[i][j] + b[i][j]);
+        printf("\n");
+    }
+
+    return 0;
+}
